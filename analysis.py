6f2b419d310d5547df22a6b69ced9b581b1ee1a0
nataliaheine <136356193+nataliaheine@users.noreply.github.com>
2023-07-08 22:06:57 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,12 +8,17 @@ df = pd.read_csv("toxic_comments_de_unprocessed.csv")
 
 def start():
     st.title("Datenanalyse & Modellauswahl")
-
-
     st.info("Dataset findet man unter folgendem [Link](https://www.kaggle.com/datasets/shubheshswain/jigsaw-toxic-comment-classification-german).")
 
-
     st.header("So sehen die ursprunglichen Daten aus:")
+    st.dataframe(df.head(10))
+
+    st.text("Das zukünftige Modell braucht nur 1 Label statt 6. Ich habe alles zu einem Label gebracht und zwar nach folgendem Prinzip: wenn ein Kommentar zumindest bei einem der Labels mit '1' markiert wurde, bekam er bei dem neuen Label auch ein '1'.  \n\nSomit wurden auch fast harmlose Kommentare als toxisch gelabelt. Dies könnte man ändern indem man bei Erstellung des neues Labels nicht alle der alten Labels beachtet, sondern z.B. nur die Features 'severe toxic', 'threat' und "insult'") 
+    st.text("Außerdem gab es im gesamten Dataset 133.559 nicht toxische und nur 15.086 toxische Kommentare, was beim Trainieren des Modells dazu führte, dass das Modell sich dafür entschied, die meisten Kommentare als nicht toxisch zu markieren und damit 95% accuracy zu erreichen")
+    st.header("So sah das neue DataFrame aus mit nur 1 Label und der gleichen Anzahl an Kommentaren jeder Klasse:")
+    df["label"] = df[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']].any(axis=1).astype(int)
+    df = df.drop(['toxic', 'severe_toxic', 'obscene', 'threat', 'insult','identity_hate'], axis=1)
+    df = df[df["label"]==0].sample(n=15100, random_state=42).append(df[df["label"]==1]).reset_index(drop=True)
     st.dataframe(df.head(10))
     
     '''
